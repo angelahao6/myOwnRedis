b188cb09267c244a14fdde7240b5f5a8f1bf3887
Angela <angelahao@g.ucla.edu>
2025-08-24 00:42:15 -0400

    Implement helper functions to read and write multiple requests

--- client.cpp
+++ client.cpp
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
 
 const size_t k_max_msg = 4096;
 
@@ -17,15 +18,33 @@ static void die(const char* msg) {
 }
 
 static void msg(const char *msg) {
-    // todo: implement this function
+    fprintf(stderr, "%s\n", msg);
 }
 
 static int32_t read_full(int fd, char *buf, size_t n) {
-    // todo: implement this function
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 static int32_t write_all(int fd, const char *buf, size_t n) {
-    // todo: implement this function
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 static int32_t query(int fd, const char *text) {
