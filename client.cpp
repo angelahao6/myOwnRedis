c537e98e3c6825c89ed6d96b66659884e66349cb
Angela <angelahao@g.ucla.edu>
2025-08-24 00:22:00 -0400

    Implement reading and writing multiple requests for client

--- client.cpp
+++ client.cpp
@@ -8,10 +8,64 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+const size_t k_max_msg = 4096;
+
 static void die(const char* msg) {
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
+}
+
+static void msg(const char *msg) {
+    // todo: implement this function
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    // todo: implement this function
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    // todo: implement this function
+}
+
+static int32_t query(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+
+    // send the request
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+        return err;
+    }
+
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    
+    // read the 4 bytes header
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // read the reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    printf("server says: %.*s\n", len, &rbuf[4]);
+    return 0;
 }
 
 int main() {
